ce45ca19f973f9894fcba84dd689ba9c31ac3013
kbugz <kbugusky@gmail.com>
2022-02-22 12:22:02 -0600

    inquirer set up and call

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const fs = require('fs'); // imports the fs module into the project
-const generatePage = require('./src/page-template'); // locally written generate page module loaded in. 
-const profileDataArgs = process.argv.slice(2);
-// Lets see the console log of profileDataArgs
-console.log(profileDataArgs);
+const inquirer = require('inquirer'); // imports the inquirer module into the project
+// const fs = require('fs'); // imports the fs module into the project
+// const generatePage = require('./src/page-template'); // locally written generate page module loaded in. 
 
-// Lets see the console log of profileDataArgs
-const [name, github] = profileDataArgs;
-console.log(name, github);
 
-// function generatePage with two parameter input
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 
-// Writing the file
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
